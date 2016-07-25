e78b6fdb6280f7a36cb130c8ba3c99722ef31ad8
ccantey <cantey0701@gmail.com>
2016-07-25 15:50:53 -0500

    little results table

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var popup;
-var zoomThreshold = 8;
+
 function initialize(){
 	$("#map").height('800px');
 	southWest = new mapboxgl.LngLat( -104.7140625, 41.86956);
@@ -28,7 +28,7 @@ function initialize(){
     	// add vector source:
 	    map.addSource('2012generalelection', {
 	        type: 'vector',
-	        url: 'mapbox://ccantey.625svqgl'
+	        url: 'mapbox://ccantey.3o07lep8'
 	    });
 	    //vs add geojson source, a bit slower than vector. A php geojson source will be even slower due to browser cache:
 	    // map.addSource('hse2012_vtd2015', {
@@ -42,30 +42,10 @@ function initialize(){
 	    // });
 
 	    map.addLayer({
-	        "id": "2012results-county",
+	        "id": "2012results",
 	        "type": "fill",
 	        "source": "2012generalelection",
 	        "source-layer": "2012generalresults", //layer name in studio
-	        "min-zoom":zoomThreshold,
-	        'filter': ['==', 'COUNTYFIPS', true],
-	        "layout": {
-	            // "line-join": "round",
-	            // "line-cap": "round"
-	        },
-	        "paint": {
-	            "fill-color": "steelblue",
-	            "fill-opacity": 0.5,
-	            "fill-outline-color": "white"
-	        }
-        });
-
-        map.addLayer({
-	        "id": "2012results-mcd",
-	        "type": "fill",
-	        "source": "2012generalelection",
-	        "source-layer": "2012generalresults", //layer name in studio
-	        "min-zoom":zoomThreshold,
-	        'filter': ['==', 'COUNTYFIPS', true],
 	        "layout": {
 	            // "line-join": "round",
 	            // "line-cap": "round"
@@ -118,48 +98,39 @@ function initialize(){
     
 	});
 
-	map.on('mousemove', function (e) {
+
+    //mousemove is too slow, need to create a new layer at street level for mouseover
+	map.on('click', function (e) {
        var features = map.queryRenderedFeatures(e.point, {
        	layers:['2012results']
        }); //queryRenderedFeatures returns an array
-
-       mousehighlight(features);
-        
-
-       var feature = features[0]
-
-       // JUST GET RID OF POPUP - USE HTML CSS!! http://bl.ocks.org/hrecht/82b6440ed3b982a6f594
-       // console.log(feature.properties)
-       // var html = "County: " + feature.properties.COUNTYNAME + "<br> Congress: " + feature.properties.CONGDIST;
-       // popup = new mapboxgl.Popup()
-       //  .setLngLat(e.lngLat)
-       //  .setHTML(html)
-       //  .addTo(map);
-       // document.getElementById('features').innerHTML = JSON.stringify(features, null, 2);
-    });
-
-    map.on("mouseout", function() {
-        map.setFilter("2012results-hover", ["==", "VTD", ""]);
-    });
-
-    map.on('zoom', function() {
-	    if (map.getZoom() > zoomThreshold) {
-	        stateLegendEl.style.display = 'none';
-	        countyLegendEl.style.display = 'block';
-	    } else {
-	        stateLegendEl.style.display = 'block';
-	        countyLegendEl.style.display = 'none';
-	    }
-    });
-}
-
-function mousehighlight(features){
-	console.log(features)
-	if (features.length) {
+       
+        if (features.length) {
             map.setFilter("2012results-hover", ["==", "VTD", features[0].properties.VTD]);
         } else {
             map.setFilter("2012results-hover", ["==", "VTD", ""]);
         }
+
+       var feature = features[0];
+       console.log(feature.properties);
+       var content = '';
+
+       content += "<tr><th>Congressional District: </th><td> " + feature.properties.CONGDIST+ "</td></tr>";
+       content += "<tr><th>DFL: </th><td> " + feature.properties.USREPDFL+ "</td></tr>";
+       content += "<tr><th>Republican: </th><td> " + feature.properties.USREPR+ "</td></tr>";
+       content += "<tr><th>Independent: </th><td> " + feature.properties.USREPIP+ "</td></tr>";
+       content += "<tr><th>WI??: </th><td> " + feature.properties.USREPWI+ "</td></tr>";
+       content += "<tr><th>TOTAL: </th><td> " + feature.properties.USREPTOTAL+ "</td></tr>";
+       // popup = new mapboxgl.Popup()
+       //  .setLngLat(e.lngLat)
+       //  .setHTML(content)
+       //  .addTo(map);
+       document.getElementById('features').innerHTML = content;
+    });
+
+    // map.on("mouseout", function() {
+    //     map.setFilter("2012results-hover", ["==", "VTD", ""]);
+    // });
 }
 
 //will color similar to https://www.mapbox.com/mapbox-gl-js/example/updating-choropleth/
