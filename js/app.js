6cfc7d46cc2c8b7d94e7d6e994dfa7d0feda2928
ccantey <cantey0701@gmail.com>
2016-08-31 14:32:49 -0500

    images

--- js/app.js
+++ js/app.js
@@ -208,7 +208,7 @@ function showResults(activeTab, feature){
     //display the results in the results div
 	for (var i=0; i < resultsArray.length; i++){
 		if (resultsArray[i][1] > 0){ 
-		  content += "<tr><th>"+resultsArray[i][0]+": </th><td> " + resultsArray[i][1]+ "</td></tr>";
+		  content += "<tr><th>"+resultsArray[i][0]+": </th><td> " + resultsArray[i][1].toLocaleString()+ "</td></tr>";
 		  document.getElementById('features').innerHTML = content;
 		}
 	}
